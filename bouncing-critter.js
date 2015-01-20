2ec0835116346afcc62bd63a3f110d33367de954
Stephen Done <srdone@gmail.com>
2015-01-19 21:15:10 -0700

    Modified bouncing critter test structure

--- bouncing-critter.js
+++ bouncing-critter.js
@@ -6,7 +6,7 @@ function BouncingCritter() {
   this.direction = randomElement(directionNames);
 }
 BouncingCritter.prototype.act = function(view) {
-  if (view.look(this.direction) != " ")
+  if (view.look(this.direction) !== " ")
     this.direction = view.find(" ") || "s";
   return {type: "move", direction: this.direction};
 };
--- spec/bouncing-critter-spec.js
+++ spec/bouncing-critter-spec.js
@@ -6,15 +6,12 @@ describe("Bouning critter action", function() {
   var critter = new BouncingCritter();
   var view = {};
   var alternateDirection = new Vector(1, 1);
-  view.look = function() {return "X";};
   view.find = function() {return alternateDirection;};
-  var action = critter.act(view);
 
-  it("should return an object with type 'move'", function() {
+  it("should return an object with type 'move' with direction equal to alternateDirection", function() {
+    view.look = function() {return "X";};
+    var action = critter.act(view);
     expect(action.type).toBe("move");
-  });
-
-  it("should return an object with direction property equal to alternateDirection", function() {
     expect(action.direction).toBe(alternateDirection);
   });
 });
